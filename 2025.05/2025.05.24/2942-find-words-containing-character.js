96488064be9f1af034847c8ea6e1bc7bb368e7ef
Alexey Berezin <beraliv.spb@gmail.com>
2025-05-25 14:52:29 +0100

    feat: 2025.05.24

---
+++ 2025.05/2025.05.24/2942-find-words-containing-character.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+  const indices = [];
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (word.includes(x)) {
+      indices.push(i);
+    }
+  }
+  return indices;
+};
